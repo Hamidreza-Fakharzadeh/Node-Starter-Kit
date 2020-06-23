668c9d33c5dc14bf3908a6884f9d9e4e759a1e5d
hamidreza <hrfarzadeh@gmail.com>
2020-06-23 21:24:30 +0100

    test heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express")
 
 const app = express();
-app.get("/cho", (request, response) => {
+app.get("/myMessage", (request, response) => {
     const amount = request.query.amount;
-    response.send(`amount${amount}`)
+    response.send("Hello World")
 })
 
 app.listen(3000, function() {
